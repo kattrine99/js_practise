045adbd636e9d8f201225e979aa9fdf650b5e309
Ekaterina <kattren990703@gmail.com>
2024-01-08 11:26:19 +0500

    create functions for optimizing code

--- js/first_practice.js
+++ js/first_practice.js
@@ -1,5 +1,18 @@
-/*"use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+"use strict";
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно */
+
+let numberOfFilms;
 const personalMovieDB ={
     count: numberOfFilms , 
     movies:{},
@@ -8,8 +21,18 @@ const personalMovieDB ={
     privat: false
 };
 
-for(let i=0;i<2;i++)
-{
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilms =='' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+function rememberMyFilms(){
+
+    for(let i=0;i<2;i++)
+ {
     const a = prompt('Один из последних просмотренных фильмов?', ' '),
     b = prompt('На сколько оцените его?', ' ');
     if(a != null && b != null && a != '' && b != '' && a.length <50 )
@@ -21,8 +44,13 @@ for(let i=0;i<2;i++)
         console.log("error");
         i--;
     }
+ }
 }
-if(personalMovieDB.count <10)
+rememberMyFilms();
+
+function detectPersonalLevel(){
+
+    if(personalMovieDB.count <10)
 {
     console.log("Просмотрено довольно мало фильмов");
 }else if(personalMovieDB.count >=10 && personalMovieDB.count<30)
@@ -35,10 +63,21 @@ if(personalMovieDB.count <10)
 {
     console.log("Что то не так...");
 }
-console.log(personalMovieDB);*/
-function sayHello(UserName) {
-    console.log("Привет, ", UserName,"!");
-    return UserName;
- }
- let UserName = prompt("Как тебя зовут?", '');
- sayHello(UserName);
+}
+detectPersonalLevel();
+
+function showMyDB (hidden){
+    if (!hidden)
+    {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres (){
+    for(let i = 1; i<=3; i++){
+        personalMovieDB.genres[i-1]=prompt(`Ваш любимый жанр под номером ${i}`,'');
+    }
+}
+writeYourGenres();
