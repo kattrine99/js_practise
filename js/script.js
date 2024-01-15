dad658a09fc179481bbcf017df48c7d1414b3ffc
Ekaterina <kattren990703@gmail.com>
2024-01-15 16:54:06 +0500

    experiments with functions

--- js/script.js
+++ js/script.js
@@ -390,3 +390,58 @@ function fib(num) {
    console.log(fibArray+"");
 }
 fib(7);
+
+//------------Упражнение 10------------
+/*1) Напишите функцию showExperience, 
+которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
+
+Пример:
+
+showExperience(personalPlanPeter) => '1 month'
+
+P.S. желательно использовать деструктуризацию, но не обязательно
+
+2) Напишите функцию showProgrammingLangs, 
+которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+
+Пример:
+
+showProgrammingLangs(personalPlanPeter)  =>
+
+"Язык js изучен на 20% Язык php изучен на 10%"
+
+Причем функция должна работать вне зависимости от количества языков. 
+Если ни один не указан, то возвращается пустая строка.
+
+P.S. Для переноса строки используется \n в конце строки.
+
+3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter.
+ При его вызове метод будет принимать в себя объект и возвращать строку в нужном виде.
+
+Пример:
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+=> 'Мне 29 и я владею языками: RU ENG' */
+const personalPlanPeter = {
+   name: "Peter",
+   age: "29",
+   skills: {
+       languages: ['ru', 'eng'],
+       programmingLangs: {
+           js: '20%',
+           php: '10%'
+       },
+       exp: '1 month'
+   }
+};
+const {languages,programmingLangs,exp } = personalPlanPeter.skills;
+
+function showExperience(plan) {
+   plan = console.log(exp);
+   return plan;
+}
+
+function showProgrammingLangs(plan) {
+
+}
+showExperience(personalPlanPeter);
