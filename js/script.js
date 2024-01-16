edf2cb1113d5ae34b481902571676a316aa655b2
Ekaterina <kattren990703@gmail.com>
2024-01-16 19:16:33 +0500

    adding the ex#11

--- js/script.js
+++ js/script.js
@@ -453,3 +453,40 @@ function showProgrammingLangs(plan) {
 }
 showExperience(personalPlanPeter);
 showProgrammingLangs(showProgrammingLangs);
+//------------Упражнение 11------------
+/*1) Напишите функцию showFamily,
+ которая будет принимать в себя массив строк и 
+ возвращать сообщение в нужном формате.
+
+showFamily(family)  => 'Семья состоит из:
+ Peter Ann Alex Linda'
+
+Имена подставляются автоматически из массива. 
+Если массив пустой, то 
+выводится сообщение 'Семья пуста'
+2) напишите функцию standardizeStrings,
+ которая будет принимать в себя массив строк
+  и будет выводить в консоль эти строки 
+  в нижнем регистре.
+
+Пример:
+
+standardizeStrings(favoriteCities)  
+выведет в консоль
+lisbon
+rome
+milan 
+*/
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    arr.join(" ");
+    
+}
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    
+}
