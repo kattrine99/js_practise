1a07f2f3dd5f2464ab17c75aa8e2bb2f25c2bda7
Ekaterina <kattren990703@gmail.com>
2024-01-27 16:00:13 +0500

    solved problems for better understanding basics of js

--- js/script.js
+++ js/script.js
@@ -686,3 +686,54 @@ function sortStudentsByGroups(arr) {
    
 }
 sortStudentsByGroups(students);
+
+
+
+//________________Упражнения с собеседований на понимание основ________________
+
+// •	Какое будет выведено значение: let x = 5; alert( x++ ); ?
+let j=5;
+alert(x++); //Ответ: выдаст окно с 6
+
+// •  Чему равно такое выражение: [ ] + false - null + true ?
+
+console.log([ ]+false-null+true); //NaN
+// •	Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+
+let y =1;
+let x=y=2;
+console.log(x); //Ответ: выдаст окно с 2 
+// •	Чему равна сумма [ ] + 1 + 2?
+
+console.log([ ]+1+2); // Ответ: выдаст окно с 12
+// •	Что выведет этот код: alert( "1"[0] )?
+
+console.log("1"[0]); // Ответ: выдаст окно с 1
+// •	Чему равно 2 && 1 && null && 0 && undefined ?
+
+console.log(2 && 1 && null && 0 && undefined); // Ответ: null
+// •	Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+// Ответ: первое выражение выйдет как булеан операция а второе просто если и то и то выполняется.
+// •	Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
+
+console.log(null || 2 && 3 || 4); //3
+// •	a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+
+let f = [1, 2, 3], d = [1, 2, 3];
+if(f==d)
+{
+   console.log("Correct");
+}
+else{
+   console.log("Unequal");
+}// как выдает консоль они не равны
+
+// •	Что выведет этот код: alert( +"Infinity" ); ?
+
+console.log(+"Infinity"); // Infinity
+// •	Верно ли сравнение: "Ёжик" > "яблоко"?
+
+console.log("Ёжик" > "яблоко"); // false
+// •	Чему равно 0 || "" || 2 || undefined || true || falsе ?
+
+console.log(0 || "" || 2 || undefined || true || falsе);//2
