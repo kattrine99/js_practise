d3795246690c4e87c3d75fe3ce75ce80fb3f0597
Ekaterina <kattren990703@gmail.com>
2024-01-29 20:16:23 +0500

    practice with document

---
+++ elements/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const box = document.getElementById('box');
+
+const btns = document.getElementsByTagName('button');
+
+const circles = document.getElementsByClassName('circle');
+
+const hearts = document.querySelectorAll('.heart');
+
+const OneHeart = document.querySelector('.heart');
+
+
+box.style.cssText = 'background-color: blue;width: 500px';
+btns[1].style.borderRadius = '100%';
+
